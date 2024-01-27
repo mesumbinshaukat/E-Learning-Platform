f4dd9b15c8123e542c15ecb4220e1d9f009261c3
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-01-27 17:35:22 +0500

    Updated

--- index.php
+++ index.php
@@ -17,11 +17,43 @@
         <?php include("./partials/navbar.php"); ?>
     </section>
 
-    <h1 class=" flex items-center text-5xl font-bold dark:text-dark">Hello</h1>
+    <section class="section ">
+        <div class="container">
+            <div class="row align-items-center justify-content-lg-between justify-content-sm-center">
+                <div class="col-md-6 col-lg-6 mb-4 mb-md-4 mb-lg-0">
+                    <div class="hero-slider-content">
+                        <h1 class="display-2">Welcome to <br> TriaRight
+                        </h1>
+                        <p class="lead">TriaRight is a hub with the largest learning technologies community on your
+                            side. Join today and get access to thousands of live and on-demand learning resources with
+                            Internships in place and In addition with Job opportunities — all designed to help you take
+                            your professional development further.</p>
+                        <a href="https://triaright.com/jobslist.php" class="btn btn-secondary mt-3">Jobs</a>
+                        <a href="https://triaright.com/internshipslist.php"
+                            class="btn btn-secondary mt-3">Internships</a>
+                        <a href="https://triaright.com/courseslist.php" class="btn btn-secondary mt-3">Courses</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-5">
+                    <div>
+
+                        <img class=" " src="https://triaright.com/website/assets/img/bird&#32;(1).png"
+                            alt="modern desk">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
     <?php include("./partials/footer.php");
     ?>
+
+
+</body>
+
+</html>
 
 
 </body>
