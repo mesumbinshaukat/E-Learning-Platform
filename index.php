99f21d5a39b7b5ef8d5c4d354b09717415c60b33
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-01-27 14:06:39 +0500

    Updated

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include("./partials/links.php")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <section>
+        <navbar>
+            <?php include("./partials/navbar.php"); ?>
+        </navbar>
+
+    </section>
+
+    <h1 class="flex items-center text-5xl font-bold dark:text-dark">Hello</h1>
+
+</body>
+
+</html>
