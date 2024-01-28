7b6ccab455873ce4f51e4ed4ed3b3f24fcfa3fde
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-01-29 02:41:13 +0500

    Updated

--- db_connection/connection.php
+++ db_connection/connection.php
@@ -5,3 +5,9 @@ $database = "e-learning";
 $password = "";
 
 $conn = mysqli_connect($server, $id, $password, $database) or die("Connection Failed");
+// $server = "sql207.infinityfree.com";
+// $id = "if0_35870820";
+// $database = "if0_35870820_e_learning";
+// $password = "EpxotIyvIo";
+
+// $conn = mysqli_connect($server, $id, $password, $database) or die("Connection Failed");
