c59bae4f0562b69fca35032fc02d122bed64029a
abdulrafayKhan-10 <120657096+abdulrafayKhan-10@users.noreply.github.com>
2024-01-28 20:39:43 +0500

    all register's page

---
+++ studentregister.php
@@ -0,0 +1,313 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+ 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+     <link rel="shortcut icon" href="../assets/img/web/icon.png">
+    <!--<link rel="shortcut icon" href="assets/img/web/icon.png" type="image/png" sizes="16x16">-->
+
+    
+    <title>TriaRight : The New Era of Learning</title>
+
+    
+    <link rel="stylesheet" href="assets/css/main.css">
+   
+</head>
+
+<body>
+
+    <!--preloader start-->
+    <!-- <div id="preloader">
+        <div class="loader1">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div> -->
+    <!--preloader end-->
+    <!--header section start-->
+     <header class="header position-relative z-9">
+        <nav class="navbar navbar-expand-lg navbar-light navbar-theme-white fixed-top headroom">
+            <div class="container position-relative">
+                <a class="navbar-brand mr-lg-3" href="index.php">
+                                       <img class="navbar-brand-dark" src="assets/img/web/logoblack.png" alt="menuimage">
+                    <img class="navbar-brand-light" src="assets/img/web/logowhite.png" alt="menuimage">
+                </a>
+                <div class="navbar-collapse collapse" id="navbar-default-primary">
+                    <div class="navbar-collapse-header">
+                        <div class="row">
+                            <div class="col-6 collapse-brand">
+                                <a href="#">
+                                  <img src="assets/img/web/logoblack.png" alt="menuimage">
+                                </a>
+                            </div>
+                            <div class="col-6 collapse-close">
+                                <i class="fas fa-times" data-toggle="collapse" role="button"
+                                   data-target="#navbar-default-primary" aria-controls="navbar-default-primary"
+                                   aria-expanded="false" aria-label="Toggle navigation"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="navbar-nav navbar-nav-hover ml-auto">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+						 <li class="nav-item">
+                            <a class="nav-link" href="about.php">About</a>
+                        </li>
+						 <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                                <span class="nav-link-inner-text">Services</span>
+                                <i class="fas fa-angle-down nav-link-arrow ml-1"></i>
+                            </a>
+                            <ul class="sub-menu dropdown-menu">
+                                <li><a class="dropdown-item" href="jobs.php">Jobs</a></li>
+                                <li><a class="dropdown-item" href="internships.php">Internships</a></li>
+                                <li><a class="dropdown-item" href="courses.php">Courses</a></li>
+                               
+                            </ul>
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="jobslist.php">Jobs</a>
+                        
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="internshipslist.php">Internships</a>
+                        
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="courseslist.php">Courses</a>
+                        
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="careers.php">Careers</a>
+                        
+                        </li>
+						
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link" data-toggle="dropdown" role="button">
+                                <span class="nav-link-inner-text">Login/Register </span>
+                                <i class="fas fa-angle-down nav-link-arrow ml-1"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown-submenu">
+                                    <a href="#" class="dropdown-toggle dropdown-item d-flex justify-content-between align-items-center" aria-haspopup="true" aria-expanded="false">Login <i class="fas fa-angle-right nav-link-arrow"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="superadmin.php">Super admin</a></li>
+                                        <li><a class="dropdown-item" href="employee.php">Employee</a></li>
+                                        <li><a class="dropdown-item" href="students.php">Student</a></li>
+                                        <li><a class="dropdown-item" href="college.php">College</a></li>
+                                        <li><a class="dropdown-item" href="collegementor.php">College-mentor</a></li>
+                                        <li><a class="dropdown-item" href="trainer.php">Trainer</a></li>
+                                        <li><a class="dropdown-item" href="company.php">Company</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown-submenu">
+                                    <a href="#" class="dropdown-toggle dropdown-item d-flex justify-content-between align-items-center" aria-haspopup="true" aria-expanded="false">Register<i class="fas fa-angle-right nav-link-arrow"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="studentregister.php">Student</a></li>
+                                        <li><a class="dropdown-item" href="collegeregister.php">College</a></li>
+                                        <li><a class="dropdown-item" href="companyregister.php">Company</a></li>
+                                        <li><a class="dropdown-item" href="trainerregister.php">Trainer</a></li>
+                      
+                                    </ul>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                       
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact Us</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <div class="d-flex align-items-center">
+                    <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbar-default-primary" aria-controls="navbar-default-primary" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div> 
+            </div>
+        </nav>
+    </header>
+    <!--header section end-->
+
+    <div class="main">
+
+        <!--sign up section start-->
+        
+        <section class="section section-lg section-header position-relative min-vh-100 flex-column d-flex justify-content-center">
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    
+                    <div class="col-md-5 col-lg-15">
+                        <div class="card login-signup-card shadow-lg mb-0">
+                            <div class="card-body px-md-5 py-5">
+                                <div class="mb-5">
+                                    <h3>Create Account</h3>
+                                    <p class="text-muted">Sign in to your account to continue.</p>
+                                </div>                                <!--sign up form-->
+								
+                                <form class="login-signup-form" action="" method="POST">
+								
+                                    <div class="form-group">
+                                        <!-- Label -->
+                                        <label class="font-weight-bold">
+                                            Student Name
+                                        </label>
+                                        <!-- Input group -->
+                                        <div class="input-group input-group-merge">
+                                           
+                                        <input type="text" name="student_name" class="form-control" placeholder="Enter your name" required>
+                                        </div>
+                                    </div>
+									
+									 <div class="form-group">
+                                        <!-- Label -->
+                                        <label class="font-weight-bold">
+                                           Phone No
+                                        </label>
+                                        <!-- Input group -->
+                                        <div class="input-group input-group-merge">
+                                           
+                                        <input type="text" maxlength="10" pattern=[0-9]{1}[0-9]{9} name="phone_number" class="form-control" placeholder="1234567890" required  required >
+                                     
+                                        </div>
+                                    </div>
+									
+									
+                                    <div class="form-group">
+                                        <!-- Label -->
+                                        <label class="font-weight-bold">
+                                            Email Address
+                                        </label>
+                                        <!-- Input group -->
+                                        <div class="input-group input-group-merge">
+                                            
+                                        <input type="email" name="mail_id" class="form-control" placeholder="name@address.com" required>
+                                        </div>
+                                    </div>
+
+									<div class="form-group">
+                                        <!-- Label -->
+                                        <label class="font-weight-bold">
+                                            Student Username
+                                        </label>
+                                        <!-- Input group -->
+                                        <div class="input-group input-group-merge">
+                                            
+                                        <input type="text" name="student_username" class="form-control" placeholder="Enter username" required minlength=8 maxlength=16 required>
+                                        </div>
+                                    </div>
+											<div class="form-group">
+												<label for="exampleInputQualification">Internship Term </label>
+<select  class="form-control form-select select2" name="intership_term" data-bs-placeholder="select Internship Term" required>
+											<option value="short_term">Short Term</option>
+											<option value="long_term">Long Term</option>
+										</select>
+											</div>
+											
+									
+									<div class="form-group">
+                                        <!-- Label -->
+                                        <label class="font-weight-bold">
+                                            Password
+                                        </label>
+                                        <!-- Input group -->
+                                        <div class="input-group input-group-merge">
+                                            
+                                        <input type="password" name="password" class="form-control" id='password' placeholder="Enter your password" minlength=8 maxlength=10 required>
+                                        </div>
+                                    </div>
+									
+									<div class="form-group">
+                                        <!-- Label -->
+                                        <label class="font-weight-bold">
+                                            Re-Enter password
+                                        </label>
+                                        <!-- Input group -->
+                                        <div class="input-group input-group-merge">
+                                            
+                                        <input type="password" class="form-control" id='retypepassword' placeholder="Re-Enter your password" minlength=8 maxlength=10 required>
+                                        </div>
+                                    </div>
+									
+								
+									
+									
+
+                                    <div>
+  <input type="checkbox" name="checkbox" id="send_newsletter" required/>
+ By clicking Agree and Join, you agree to the TriaRight <a href="useragreement.html"> User Agreement</a> and <a href="privacypolicy.html"> Privacy policy</a>
+  </div>
+
+                                    <!-- Submit -->
+                                    <button class="btn btn-block btn-secondary border-radius mt-4 mb-3" name="submit" value="submit" type="submit" onclick="return check()">
+                                        Agree & Join
+                                    </button>
+                                </form>
+                                <script type="text/javascript">
+	function check(){
+	var b=document.getElementById('password').value;
+	var c=document.getElementById('retypepassword').value;
+	if(b!=c){
+	    alert('Password doesnt match');
+	    		return false;
+	}
+	
+
+	else
+		return true;
+}
+</script>
+
+                                    </div>
+                            <div class="card-footer bg-soft text-center border-top px-md-5"><small>Already registered?</small>
+                                <a href="students.php" class="small"> Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--sign up section end-->
+
+
+    </div>
+
+    <!--scroll bottom to top button start-->
+    <button class="scroll-top scroll-to-target" data-target="html">
+        <span class="fas fa-hand-point-up"></span>
+    </button>
+    <!--scroll bottom to top button end-->
+    <!--build:js-->
+    <script src="assets/js/vendors/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/vendors/popper.min.js"></script>
+    <script src="assets/js/vendors/bootstrap.min.js"></script>
+    <script src="assets/js/vendors/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/vendors/jquery.easing.min.js"></script>
+    <script src="assets/js/vendors/mixitup.min.js"></script>
+    <script src="assets/js/vendors/headroom.min.js"></script>
+    <script src="assets/js/vendors/smooth-scroll.min.js"></script>
+    <script src="assets/js/vendors/wow.min.js"></script>
+    <script src="assets/js/vendors/owl.carousel.min.js"></script>
+    <script src="assets/js/vendors/jquery.waypoints.min.js"></script>
+    <!--<script src="assets/js/vendors/countUp.min.js"></script>-->
+    <script src="assets/js/vendors/jquery.countdown.min.js"></script>
+    <script src="assets/js/vendors/validator.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    <!--endbuild-->
+</body>
+
+
+<!-- Mirrored from corporx.themetags.com/basic-sign-up.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Jan 2023 13:39:06 GMT -->
+</html>
