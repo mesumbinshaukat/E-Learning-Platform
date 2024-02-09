80fdd3a882c5b6d64af2c349fdf101d19c50b9eb
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-02-10 02:26:20 +0500

    Updated

--- superadmin/batch_manage.php
+++ superadmin/batch_manage.php
@@ -8,6 +8,28 @@ if (!isset($_COOKIE['superadmin_username']) && !isset($_COOKIE['superadmin_passw
     exit();
 }
 
+if (isset($_POST["update"])) {
+
+    $id = $_POST["id"];
+    $batch_name = $_POST["batch_name"];
+    $class_duration = $_POST["class_duration"];
+    $batch_starting_date = $_POST["batch_starting_date"];
+    $batch_ending_date = $_POST["batch_ending_date"];
+    $session_slot = $_POST["update_session_slot"];
+    $additional_info = $_POST["additional_info"];
+
+    $update_query = mysqli_prepare($conn, "UPDATE `batch` SET `batch_name`=?,`class_duration`=?, `batch_starting_date`=?,`batch_ending_date`=?,`session_slot`=?,`additional_info`=? WHERE `id` = '$id'");
+    $update_query->bind_param("ssssss", $batch_name, $class_duration, $batch_starting_date, $batch_ending_date, $session_slot, $additional_info);
+
+    if ($update_query->execute()) {
+        header('location: ./managebatch.php');
+        exit();
+    } else {
+        $error = $update_query->error;
+        echo $error;
+    }
+}
+
 if (isset($_GET["id"]) && isset($_GET["status"]) && $_GET["status"] == "complete") {
     $id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);
     $id = (int)$id;
