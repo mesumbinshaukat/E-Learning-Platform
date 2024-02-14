5438528e8c8a2829c9daa012d829f6637808d097
abdulrafayKhan-10 <120657096+abdulrafayKhan-10@users.noreply.github.com>
2024-02-15 04:10:52 +0500

    Update

--- superadmin/contactformmsgs.php
+++ superadmin/contactformmsgs.php
@@ -7,6 +7,39 @@ if (!isset($_COOKIE['superadmin_username']) && !isset($_COOKIE['superadmin_passw
 	header('location: ../super-admin_login.php');
 	exit();
 }
+if(isset($_POST['sendBtn'])){
+   $msg_username = $_POST['msg_username'];
+   $msg_email = $_POST['msg_email'];
+   $msg_type = $_POST['msg_type'];
+   $msg_user_type = $_POST['msg_user_type'];
+   $msg_messages = $_POST['msg_messages'];
+  $insert_query = mysqli_prepare($conn, "INSERT INTO `messages`( `receiver_id`, `message`, `user_type`, `username`, `email`, `message_type`) VALUES (0,?,?,?,?,?)"); 
+  $insert_query->bind_param("sssss", $msg_messages, $msg_user_type, $msg_username, $msg_email, $msg_type);
+   if ($insert_query->execute()) {
+    $_SESSION['message_success'] = true;
+}
+else{
+
+    $_SESSION['message_failed'] = true;
+    $_SESSION["err_msg"] = "Unsuccessful. Please try again.";
+}
+}
+if(isset($_POST['DeleteBtn'])){
+    $del_hidden_id = $_POST['del_hidden_id'];
+    $del_hidden_username = $_POST['del_hidden_username'];
+    $del_hidden_email = $_POST['del_hidden_email'];
+    $delete_query = mysqli_query($conn,"DELETE FROM `messages` WHERE id = '$del_hidden_id'");
+    if($delete_query){
+        mysqli_query($conn,"DELETE FROM `messages` WHERE username = '$del_hidden_username' && email = '$del_hidden_email'");
+        $_SESSION['del'] = true;
+        $_SESSION['del_msg'] = "Deleted Successfully";
+        
+    }
+    else{
+        $_SESSION['del'] = false;
+        $_SESSION['del_msg'] = "Unable to Delete";
+    }
+}
 ?>
 
 
@@ -27,7 +60,35 @@ if (!isset($_COOKIE['superadmin_username']) && !isset($_COOKIE['superadmin_passw
 </head>
 
 <body class="ltr main-body app sidebar-mini">
+<script>
+        <?php
+        if (isset($_SESSION['message_success']) && $_SESSION['message_success'] == true) {
+        ?>
+            toastr.success('Successfully, Replied')
+        <?php
+            session_destroy();
+        } else if (isset($_SESSION['message_failed']) && $_SESSION['message_failed'] == true) {
+        ?>
+            toastr.error(<?php echo $_SESSION["err_msg"]; ?>)
+        <?php
+            session_destroy();
+        }
+        ?>
 
+<?php
+        if (isset($_SESSION['del']) && $_SESSION['del'] == true) {
+        ?>
+            toastr.success(<?php echo $_SESSION["del_msg"]; ?>)
+        <?php
+            session_destroy();
+        } else if (isset($_SESSION['del']) && $_SESSION['del'] == false) {
+        ?>
+            toastr.error(<?php echo $_SESSION["del_msg"]; ?>)
+        <?php
+            session_destroy();
+        }
+        ?>
+    </script>
     <!-- Switcher -->
     <div class="switcher-wrapper">
         <div class="demo_changer">
@@ -448,10 +509,34 @@ if (!isset($_COOKIE['superadmin_username']) && !isset($_COOKIE['superadmin_passw
                                             <td><?php echo $row['email'] ?></td>
                                             <td><?php echo $row['message'] ?></td>
                                             <td></td>
-                                            <td> <button data-toggle="modal" data-target="#exampleModal" class="btn btn-danger">Reply</button> </td>
-                                        </tr>
+                                            <td>
+                                                <?php 
+                                                $un = $row['username'];
+                                                $em = $row['email'];
+                                                $select_msgs = mysqli_query($conn,"SELECT * FROM `messages` WHERE (message_type = 'contact_form') && (user_type = 'Admin') && (username = '$un') && (email = '$em')");
+                                                if(mysqli_num_rows($select_msgs) > 0){
+                                                ?>
 
-                                        <?php }?>
+                                                <button disabled class="btn btn-success mt-3 mb-0 "
+                                                    style="text-align:right">Replied</button>
+                                                    <form action="" method="post">
+                                                <button type="submit" name="DeleteBtn" class="btn btn-info mt-3 mb-0 " style="text-align:right">Delete</button>
+                                                <input type="hidden" value="<?php echo $row['id']?>" name="del_hidden_id">
+                                                <input type="hidden" value="<?php echo $un?>" name="del_hidden_username">
+                                                <input type="hidden" value="<?php echo $em?>" name="del_hidden_email">
+</form>
+                                                <?php } else {?>
+                                                    
+                                                    <button type="submit" name="submit" class="btn btn-info mt-3 mb-0 replyBtn"
+                                                    data-bs-target="#send" data-username="<?php echo $row['username'] ?>" data-email="<?php echo $row['email'] ?>" data-bs-toggle="modal"
+                                                    style="text-align:right">Reply</button>
+
+                                                <?php }?>
+
+                                            </td>
+                                        </tr>
+                                        <?php    
+                                         }?>
                                     </table>
                                 </div>
                             </div>
@@ -471,35 +556,79 @@ if (!isset($_COOKIE['superadmin_username']) && !isset($_COOKIE['superadmin_passw
 
 
     </div>
+    <form action="" onsubmit="return Validate_contactform()" method="post">
     <!-- End Page -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">User Details</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-            <label for="">Username</label>
-            <input type="text" class="form-control mb-2" name="lc_username" id="lc_username">
-            <p id="lc_error_username" style="color: red;"></p>
-            <label for="">Email</label>
-            <input type="email" class="form-control" name="lc_email" id="lc_email">
-            <p id="lc_error_email" style="color: red;"></p>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <input type="submit" name="lc_modalBtn" class="btn btn-primary"/>
+    <div class="modal fade" id="send">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content modal-content-demo">
+                <div class="modal-header">
+                    <h6 class="modal-title">Reply with the Message</h6><button aria-label="Close" class="btn-close"
+                        data-bs-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" name="msg_username" class="form-control mb-2" id="msg_username">
+                    <input type="hidden" name="msg_email" class="form-control mb-2" id="msg_email">
+                    <input type="hidden" name="msg_type" class="form-control mb-2" value="contact_form">
+                    <input type="hidden" name="msg_user_type" class="form-control mb-2" value="Admin">
+                    <label for="">Message</label>
+          <textarea name="msg_messages" id="msg_messages" class="form-control"  rows="3" placeholder="Enter Your Messages Here ..."></textarea>
+            <p id="msg_messages_error" style="color: red;"></p>
+           
+                  
+                </div>
+                <div class="modal-footer">
+                    <button class="btn ripple btn-info" data-bs-dismiss="modal" type="button">Close</button>
+                    <input type="submit" value="Send Message" data-bs-dismiss="modal" name="sendBtn" class="btn ripple btn-success"/>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    </form>
 
-</div>
+    </div>
     <!-- BACK-TO-TOP -->
     <a href="#top" id="back-to-top"><i class="las la-arrow-up"></i></a>
     <?php include("./scripts.php"); ?>
+    <script>
+// Add event listener to all reply buttons
+document.querySelectorAll('.replyBtn').forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Retrieve user information associated with this message
+        var username = this.getAttribute('data-username');
+        var email = this.getAttribute('data-email');
+
+        // Set the fetched data in the modal input fields
+        document.getElementById('msg_username').value = username;
+        document.getElementById('msg_email').value = email;
+ 
+    });
+});
+
+
+         var errorMessage = "";
+   function Validate_contactform(){
+        var messages = document.getElementById("msg_messages").value;
+
+        if(messages == ""){
+            errorMessage = "Please Enter Username";
+            document.getElementById("msg_messages_error").innerHTML = errorMessage;
+        }
+        else{
+            document.getElementById("msg_messages_error").innerHTML = "";
+            
+        }
+        if(errorMessage == ""){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
+
+    </script>
 </body>
 
 </html>
