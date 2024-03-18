feb1b6e8cd57a28620975d4f15c8aabe1dfcfe98
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-03-19 00:20:05 +0500

    Updated

--- partials/chatsystem/send_message.php
+++ partials/chatsystem/send_message.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Store the message in the database
     // $pdo = new PDO("mysql:host=localhost;dbname=e-learning", "root", "");
-    $pdo = new PDO("mysql:host=127.0.0.1:3306;dbname=u797177118_E_learning", "u797177118_root", "elearningPlatform123");
+    $pdo = new PDO("mysql:host=127.0.0.1:3306;dbname=u612827123_e_learning", "u612827123_root", ">S4F&n$59Qr(");
     $stmt = $pdo->prepare("INSERT INTO messages (sender_id,message,user_type,username,email,message_type) VALUES (0,:message,:user,:username,:email,'Live_Chat')");
     $stmt->bindParam(':message', $message);
     $stmt->bindParam(':user', $user);
